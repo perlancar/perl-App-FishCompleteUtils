c2999c622017dbeffb8e0892c690daf7a4617689
perlancar (@pc-office) <perlancar@gmail.com>
2016-10-21 15:26:20 +0700

    Initial commit

---
+++ lib/App/FishCompleteUtils.pm
@@ -0,0 +1,25 @@
+package App::FishCompleteUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+1;
+# ABSTRACT: Utilities related to fish shell completion
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+This distribution includes the following utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+=cut
